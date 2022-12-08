4d0823bdc355cb16d0a45c9568b603a125bcdce9
ChristerT <christertornqvist@ownit.nu>
2022-12-08 17:04:28 +0100

    Bara en html-fil och JS-validation.

--- bishbosh.js
+++ bishbosh.js
@@ -1,3 +1,23 @@
+
+(function () {
+    'use strict'
+  
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+
 for (let tillHundra = 1; tillHundra < 101; tillHundra++)
         {
             if (tillHundra % 12 == 0) console.log('bish-bosh');
